ca782616f2326fc54fc3d980afb4a4eaa09a2bc5
Amdiatou TIMERA <amdiatoutimera@hotmail.fr>
2022-03-17 03:11:29 +0100

    SONAR

--- src/main/java/com/atimera/api/constant/Authority.java
+++ src/main/java/com/atimera/api/constant/Authority.java
@@ -1,8 +1,10 @@
 package com.atimera.api.constant;
 
+import static com.atimera.api.constant.Permission.*;
+
 public class Authority {
-    public static final String[] USER_AUTHORITIES = { "user:read"};
-    public static final String[] MANAGER_AUTHORITIES = { "user:read", "user:update" };
-    public static final String[] ADMIN_AUTHORITIES = { "user:create", "user:read", "user:update" };
-    public static final String[] SUPER_ADMIN_AUTHORITIES = { "user:create", "user:read", "user:update", "user:delete" };
+    public static final String[] USER_AUTHORITIES = {USER_READ};
+    public static final String[] MANAGER_AUTHORITIES = {USER_READ, USER_UPDATE};
+    public static final String[] ADMIN_AUTHORITIES = {USER_CREATE, USER_READ, USER_UPDATE};
+    public static final String[] SUPER_ADMIN_AUTHORITIES = {USER_CREATE, USER_READ, USER_UPDATE, USER_DELETE};
 }
